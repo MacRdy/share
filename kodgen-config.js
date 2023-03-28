2f410b7ef3e7243177a781ae84bef6258f98530e
Anton Shibaev <macready.dev@gmail.com>
2023-03-29 00:05:56 +0300

    Update kodgen-config.js

--- kodgen-config.js
+++ kodgen-config.js
@@ -12,11 +12,11 @@ sh.exec('git clone git@github.com:xxx.git');
 sh.cd('..');
 
 module.exports = {
-	generator: 'ng-typescript',
-	input: 'https://petstore3.swagger.io/api/v3/openapi.json',
-	output: './output',
-	clean: true,
-	templateData: `${dir}/templates`,
-	templateDataFile: `${dir}/template-data.js`,
-	hooksFile: `${dir}/hooks.js`,
+    generator: 'ng-typescript',
+    input: 'https://petstore3.swagger.io/api/v3/openapi.json',
+    output: './output',
+    clean: true,
+    templateData: `${dir}/templates`,
+    templateDataFile: `${dir}/template-data.js`,
+    hooksFile: `${dir}/hooks.js`,
 };
